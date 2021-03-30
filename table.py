6f0a4794469cb6505fc785eb70b5f371e2cac57d
hanana1253 <hanana1253@gmail.com>
2021-03-30 15:49:00 +0900

    feat: Add initializer and start_game, print_status feature
    TODO: to make game work

--- table.py
+++ table.py
@@ -1,3 +1,11 @@
 class Table:
   def __init__(self, name):
     self.name = name
+
+  def start_game(self, bet):
+    self.bet = bet
+    player.set_init()
+    bank.set_init()
+
+  def print_status(self):
+    print('[BANK] HP: {}/10, DICE: {}/15, SHIELD: {}/6'.format(bank.hp, bank.dice_left, bank.shield_left), [PLAYER] HP: {}/10, DICE: {}/15, SHIELD: {}/6'.format(player.hp, player.dice_left, player.shiled_left), sep='\n')
